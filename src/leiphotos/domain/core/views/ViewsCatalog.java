822914948ec36339375494bd41345e320872f264
Erickson Cacondo <49413799+EricksonDaGama@users.noreply.github.com>
2024-04-15 18:49:31 +0100

    Update ViewsCatalog to implement IViewsCatalog with corrected methods.

--- src/leiphotos/domain/core/views/ViewsCatalog.java
+++ src/leiphotos/domain/core/views/ViewsCatalog.java
@@ -5,10 +5,33 @@ import leiphotos.domain.core.TrashLibrary;
 
 //Class automatically generated so the code compiles
 //CHANGE ME
-public class ViewsCatalog extends IViewsCatalog {
+public class ViewsCatalog implements IViewsCatalog {
+    private MainLibrary mainLibrary;
+    private TrashLibrary trashLibrary;
 
-	public ViewsCatalog(MainLibrary mainLib, TrashLibrary trashLib) {
-		// TODO Auto-generated constructor stub
-	}
+    public ViewsCatalog(MainLibrary mainLibrary, TrashLibrary trashLibrary) {
+        this.mainLibrary = mainLibrary;
+        this.trashLibrary = trashLibrary;
+    }
 
+    @Override
+    public List<IPhoto> getRecentPhotos() {
+        return mainLibrary.getPhotos().stream()
+            .sorted((p1, p2) -> p2.capturedDate().compareTo(p1.capturedDate()))
+            .limit(10)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<IPhoto> getFavoritePhotos() {
+        return mainLibrary.getPhotos().stream()
+            .filter(IPhoto::isFavourite)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<IPhoto> getDeletedPhotos() {
+        return new ArrayList<>(trashLibrary.getPhotos());
+    }
 }
+
